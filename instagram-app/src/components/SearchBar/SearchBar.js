4f480e609f2c814a82bbb87226df31da08ed59f1
Paul Abernathy <paul.abernathy7@gmail.com>
2019-05-27 12:32:57 -0400

    searchbar complete

--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -1,11 +1,33 @@
 import React from "react";
 import dummyData from "../../dummy-data";
 import CommentSection from "../CommentSection/CommentSection";
+import "./SearchBar.css";
 
 const SearchBar = props => {
   return (
     <div className="search">
-      <input placeholder="Search" type="text" />
+      <div className="ig">
+        <img
+          className="logo"
+          src="https://img.icons8.com/ios/50/000000/instagram-new-filled.png"
+        />
+        <h1 className="logo">Instagram</h1>
+      </div>
+      <input className="searchbar" placeholder="Search" type="text" />
+      <div className="icons">
+        <img
+          className="icon"
+          src="https://img.icons8.com/windows/32/000000/hearts.png"
+        />
+        <img
+          className="icon"
+          src="https://img.icons8.com/android/24/000000/compass.png"
+        />
+        <img
+          className="icon"
+          src="https://img.icons8.com/windows/32/000000/gender-neutral-user.png"
+        />
+      </div>
     </div>
   );
 };
