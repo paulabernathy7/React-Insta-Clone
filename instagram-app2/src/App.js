b85ef9427e2e9380e85479bd23ce874f0f5fbda5
Paul Abernathy <paul.abernathy7@gmail.com>
2019-06-05 21:21:50 -0400

    setting up app

---
+++ instagram-app2/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "./logo.svg";
+import dummyData from "./dummy-data";
+
+import "./App.css";
+
+function App() {
+  return <div className="App" />;
+}
+
+export default App;
